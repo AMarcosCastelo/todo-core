2fd5fa00d5131fdd5c9ec40b31de9a8fa239b2e4
Antonio Marcos <marcos-12marcos@hotmail.com>
2023-02-04 18:58:10 -0300

    refac: theme helpers

---
+++ apps/todo-web/src/constants/theme.ts
@@ -0,0 +1,15 @@
+import { type Theme } from '@nextui-org/react';
+
+export const LIGHT_THEME: Theme = {
+  type: 'light',
+  theme: {
+    fonts: {},
+  },
+};
+
+export const DARK_THEME: Theme = {
+  type: 'dark',
+  theme: {
+    fonts: {},
+  },
+};
---
+++ apps/todo-web/src/utils/helpers/theme.ts
@@ -0,0 +1,7 @@
+import { createTheme } from '@nextui-org/react';
+
+import { DARK_THEME, LIGHT_THEME } from '../../constants';
+
+export const lightTheme = createTheme(LIGHT_THEME);
+
+export const darkTheme = createTheme(DARK_THEME);
