540bf0c51f0f195f3727696315152ca18c0d6767
Antonio Marcos <marcos-12marcos@hotmail.com>
2023-02-06 20:12:43 -0300

    feat: create SunIcon component

---
+++ apps/todo-web/src/components/SunIcon/__tests__/SunIcon.spec.tsx
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+
+import { baseRender } from '../../../utils';
+import SunIcon from '../SunIcon';
+
+describe('SunIcon', () => {
+  it('should render correctly', () => {
+    const screenLight = baseRender(<SunIcon />);
+    expect(screenLight.baseElement).toMatchSnapshot();
+
+    const screenDark = baseRender(<SunIcon />, 'dark');
+    expect(screenDark.baseElement).toMatchSnapshot();
+  });
+
+  it('should render filled icon if filled prop is passed', () => {
+    const screen = baseRender(<SunIcon filled />);
+
+    expect(screen.getByTestId('filled-icon')).toBeInTheDocument();
+    expect(screen.baseElement).toMatchSnapshot();
+  });
+});
